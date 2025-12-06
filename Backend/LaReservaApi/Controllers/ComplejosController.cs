dde4c376a9b2713d91c582b7c972ff04fd362ffa
fautaro <fautaro@gmail.com>
2025-12-06 11:55:38 -0300

    Add WebApi basica

---
+++ Backend/LaReservaApi/Controllers/ComplejosController.cs
@@ -0,0 +1,27 @@
+﻿using LaReservaApi.Models.Complejos;
+using LaReservaBackend.Application.Complejos.Queries.GetComplejos;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaReservaApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ComplejosController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public ComplejosController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] GetComplejosRequestDTO? input)
+    {
+        var request = new GetComplejosRequest(input?.ComplejoId ?? 0, input?.DeporteId ?? 0);
+        var response = await _mediator.Send(request);
+
+        return Ok(response);
+    }
+}
