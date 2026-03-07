765e3a33a49b13a93da7c607000afa46604dbb67
fautaro <fautaro@gmail.com>
2026-03-07 15:02:14 -0300

    deploy

---
+++ src/config/apiConfig.js
@@ -0,0 +1,27 @@
+const BASE_URL = 'https://fautaro.bsite.net/api'
+
+export const API_CONFIG = {
+  baseURL: BASE_URL
+}
+
+export const API_ENDPOINTS = {
+  complejos: {
+    getAll: () => `${BASE_URL}/Complejos`,
+    getById: (id) => `${BASE_URL}/Complejos/${id}`
+  },
+  reservas: {
+    getNext: (usuarioId) => `${BASE_URL}/Reserva/GetNext?UsuarioId=${usuarioId}`,
+    getHorariosDisponibles: (complejoId) => `${BASE_URL}/Reserva/HorariosDisponiblesComplejo?ComplejoId=${complejoId}`,
+    getUserReservations: (usuarioId) => `${BASE_URL}/Reserva/UserReservations?UsuarioId=${usuarioId}`,
+    cancelReservation: (id) => `${BASE_URL}/Reserva/CancelReservation/${id}`,
+    create: `${BASE_URL}/Reserva/CreateReserva`
+  },
+  usuarios: {
+    getProfile: (id) => `${BASE_URL}/Usuario/${id}`
+  },
+  medioPagos: {
+    getAll: () => `${BASE_URL}/MedioPago`
+  }
+}
+
+export default API_CONFIG
